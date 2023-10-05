6186b7fcc4f3eb4f417249e953affc0db56ff69f
David Jennings <djennings5674@gmail.com>
2023-10-04 20:16:36 -0400

    Working on course scraper

---
+++ coursescraper/scraper.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+from course import Course
+
+def scrapeCompSciCourses():
+    url = "https://catalog.odu.edu/courses/cs/"
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    results = soup.find(id="undergraduatecoursestextcontainer")
+
+    course_elements = results.find_all("div", class_="courseblock") #these are the individual courses blocks
+
+    courses = []
+    for element in course_elements:
+        number = element.find("span", class_="detail-xrefcode").text
+        title = element.find("span", class_="detail-title").text
+        hours = element.find("span", class_="detail-hours_html").text
+        detail = element.find("p", class_="courseblockextra").text
+        prereqs = element.find("span", class_="detail-prereq")
+
+        course = Course()
+        course.parseCourse(number, title, hours, detail)
+        courses.append(course)
+
+    for course in courses:
+        course.printCourse()
